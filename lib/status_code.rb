97f91b041bbbdb991f3579e6e1144083d10c158e
Sergey Semaschyk <Sergey.Semaschyk@bepaid.by>
2017-01-23 14:42:53 +0300

    add gateway support

--- lib/status_code.rb
+++ lib/status_code.rb
@@ -3,20 +3,25 @@ require 'i18n'
 require 'pry'
 
 class StatusCode
-  attr_reader :code, :locale
+  attr_reader :code, :locale, :gateway
 
   DEFAULT_LOCALE = :en
   LOCALES = [:en, :ru].freeze
 
-  def initialize(code, locale = DEFAULT_LOCALE)
+  def initialize(code, options = {})
     @code = code.to_s
-    @locale = locale.to_sym if locale
+    @locale = options[:locale].to_sym.downcase if options.key?(:locale)
+    @gateway = options[:gateway].to_s.downcase if options[:gateway]
     set_locales_settings
     set_locale
   end
 
   def decode(receiver)
-    I18n.t("#{receiver}.#{code}") if I18n.exists?("#{receiver}.#{code}")
+    if gateway && I18n.exists?("#{receiver}.#{gateway}.#{code}")
+      I18n.t("#{receiver}.#{gateway}.#{code}")
+    elsif I18n.exists?("#{receiver}.#{code}")
+      I18n.t("#{receiver}.#{code}")
+    end
   end
 
   private
@@ -28,7 +33,7 @@ class StatusCode
   end
 
   def set_locale
-    if LOCALES.include?(locale)
+    if locale && LOCALES.include?(locale)
       I18n.locale = locale
     else
       I18n.locale = DEFAULT_LOCALE
