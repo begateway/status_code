c964fbdbbd28fc9c19c22cb6ba4b01b0f7f47edb
Sergey Semaschyk <Sergey.Semaschyk@bepaid.by>
2017-02-23 18:13:34 +0300

    refactor code

--- lib/status_code.rb
+++ lib/status_code.rb
@@ -9,23 +9,16 @@ module StatusCode
 
   def self.decode(code, options = {})
     if code
-      if options
-        receiver = options[:receiver] || :customer
-        gw       = options[:gateway].to_s.downcase
-        locale   = options[:locale] || :en
-        find_message("#{receiver}.#{gw}.#{code}", "#{receiver}.#{code}", locale)
-      else
-        message("customer.#{code}", :en)
-      end
+      receiver = options[:receiver] || :customer
+      gw       = options[:gateway].to_s.downcase
+      locale   = options[:locale] || :en
+
+      message("#{receiver}.#{gw}.#{code}", locale) ||
+        message("#{receiver}.#{code}", locale)
     end
   end
 
   private_class_method
-
-  def self.find_message(gw_msg_path, general_msg_path, locale)
-    message(gw_msg_path, locale) || message(general_msg_path, locale) ||
-      message(gw_msg_path, :en) || message(general_msg_path, :en)
-  end
 
   def self.message(path, locale)
     I18n.t(path, locale: locale) if I18n.exists?(path, locale)
