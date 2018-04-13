9d37f6c91896f1b498c00bc2e478f63779f3e652
Egor Shpalov <gudel98@github.com>
2018-04-13 16:40:20 +0300

    Update

--- lib/status_code.rb
+++ lib/status_code.rb
@@ -2,18 +2,17 @@
 require 'i18n'
 
 module StatusCode
-  DEFAULT_ERROR_CODE   = '999'
-  DEFAULT_SUCCESS_CODE = '000'
+  DEFAULT_ERROR_CODE = '999'
   LOCALES_PATH = "#{__dir__}/status_code/locales/*.yml".freeze
   I18n.enforce_available_locales = false
   I18n.load_path += Dir[LOCALES_PATH]
   I18n.backend.load_translations
 
   def self.decode(code, options = {})
+    code     = DEFAULT_ERROR_CODE if code.to_s.empty?
     receiver = options[:receiver] || :customer
     gw       = options[:gateway]
     locale   = options[:locale] || :en
-    code     = status(options[:successful], code) || code
 
     message("#{receiver}.#{gw}.#{code}", locale) ||
       message("#{receiver}.#{code}", locale)
@@ -23,13 +22,5 @@ module StatusCode
     I18n.t(path, locale: locale) if I18n.exists?(path, locale)
   end
 
-  def self.status(successful_transaction, bank_code)
-    if successful_transaction
-      DEFAULT_SUCCESS_CODE
-    elsif bank_code.to_s.empty?
-      DEFAULT_ERROR_CODE
-    end
-  end
-
-  private_class_method :message, :status
+  private_class_method :message
 end
