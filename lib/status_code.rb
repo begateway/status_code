9f580f7531ad01cc198e9d6e90704af68af9fa81
Egor Shpalov <gudel98@github.com>
2018-04-13 15:27:23 +0300

    Add transaction status option

--- lib/status_code.rb
+++ lib/status_code.rb
@@ -2,25 +2,34 @@
 require 'i18n'
 
 module StatusCode
-  DEFAULT_CODE = 999
+  DEFAULT_ERROR_CODE   = '999'
+  DEFAULT_SUCCESS_CODE = '000'
   LOCALES_PATH = "#{__dir__}/status_code/locales/*.yml".freeze
   I18n.enforce_available_locales = false
   I18n.load_path += Dir[LOCALES_PATH]
   I18n.backend.load_translations
 
   def self.decode(code, options = {})
-    code = DEFAULT_CODE if code.blank?
     receiver = options[:receiver] || :customer
     gw       = options[:gateway]
     locale   = options[:locale] || :en
+    code     = status(options[:successful], code) || code
 
     message("#{receiver}.#{gw}.#{code}", locale) ||
       message("#{receiver}.#{code}", locale)
   end
 
-  private_class_method
-
   def self.message(path, locale)
     I18n.t(path, locale: locale) if I18n.exists?(path, locale)
   end
+
+  def self.status(successful_transaction, bank_code)
+    if successful_transaction
+      DEFAULT_SUCCESS_CODE
+    elsif bank_code.to_s.empty?
+      DEFAULT_ERROR_CODE
+    end
+  end
+
+  private_class_method :message, :status
 end
