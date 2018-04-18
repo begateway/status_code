3bb751dd75509b62297f223ad649b9d49f28e5f8
Egor Shpalov <gudel98@github.com>
2018-04-18 16:24:04 +0300

    Update status_code.rb

--- lib/status_code.rb
+++ lib/status_code.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 require 'i18n'
 
-module StatusCode
+class StatusCode
   APPROVE_CODE = '000'
   DECLINE_CODE = '999'
   LOCALES_PATH = "#{__dir__}/status_code/locales/*.yml".freeze
@@ -11,33 +11,37 @@ module StatusCode
 
   def self.decode(code, opts = {})
     opts[:receiver] ||= :customer
-    opts[:locale] ||= :en
-
-    return_message(code, opts)
+    opts[:locale]   ||= :en
+    new(code, opts).return_message
   end
 
-  def self.message(path, locale)
-    I18n.t(path, locale: locale) if I18n.exists?(path, locale)
+  def initialize(code, opts)
+    @code = code
+    @opts = opts
   end
 
-  def self.find_message(code, opts)
-    message("#{opts[:receiver]}.#{opts[:gateway]}.#{code}", opts[:locale]) ||
-      message("#{opts[:receiver]}.#{code}", opts[:locale])
-  end
-
-  def self.code_blank(code, opts, role)
-    opts[:receiver] == role and (code.to_s.empty? || find_message(code, opts).nil?)
-  end
-
-  def self.return_message(code, opts)
-    return if code_blank(code, opts, :merchant)
-    default_code = opts[:status] ? APPROVE_CODE : DECLINE_CODE
-    if code_blank(code, opts, :customer)
-      find_message(default_code, opts)
+  def return_message
+    return if code_blank(:merchant)
+    default_code = @opts[:status] ? APPROVE_CODE : DECLINE_CODE
+    if code_blank(:customer)
+      find_message(default_code)
     else
-      find_message(code, opts)
+      find_message(@code)
     end
   end
 
-  private_class_method :message, :find_message, :code_blank, :return_message
+  private
+
+  def message(path)
+    I18n.t(path, locale: @opts[:locale]) if I18n.exists?(path, @opts[:locale])
+  end
+
+  def find_message(code)
+    message("#{@opts[:receiver]}.#{@opts[:gateway]}.#{code}") ||
+      message("#{@opts[:receiver]}.#{code}")
+  end
+
+  def code_blank(role)
+    @opts[:receiver] == role and (@code.to_s.empty? || find_message(@code).nil?)
+  end
 end
